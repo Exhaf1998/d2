cee2aa45d199926fed5011dccc85a4e2971e291e
fwcd <fwcdmail@gmail.com>
2020-10-17 02:17:13 +0200

    Replace iCalendarKit by swift-icalendar

--- Package.swift
+++ Package.swift
@@ -22,6 +22,7 @@ let package = Package(
         .package(url: "https://github.com/fwcd/swift-utils.git", from: "1.1.0"),
         .package(url: "https://github.com/fwcd/swift-graphics.git", .revision("206b619632fe7a2b6503ebf562c49949d2b2749f")),
         .package(url: "https://github.com/fwcd/swift-gif.git", .revision("8503ea532f0d48e6213de263b598f2caf17fd7be")),
+        .package(url: "https://github.com/fwcd/swift-icalendar.git", .revision("b0d2972d9f92788710eb3eb48da05c57a6bc1401")),
         .package(url: "https://github.com/swift-server/swift-backtrace.git", from: "1.1.1"),
         .package(name: "Emoji", url: "https://github.com/safx/Emoji-Swift.git", .revision("b3a49f4a9fbee3c7320591dbc7263c192244063e")),
         .package(name: "PerfectSysInfo", url: "https://github.com/PerfectlySoft/Perfect-SysInfo.git", from: "3.0.0"),
@@ -32,8 +33,7 @@ let package = Package(
         .package(url: "https://github.com/NozeIO/swift-nio-irc-client.git", from: "0.7.2"),
         .package(url: "https://github.com/MaxDesiatov/XMLCoder.git", from: "0.11.1"),
         .package(url: "https://github.com/wfreitag/syllable-counter-swift.git", .revision("1c677a1bc7ffc96843e9cd7ca2a619c34e8158b0")),
-        .package(url: "https://github.com/nmdias/FeedKit.git", from: "9.1.2"),
-        .package(url: "https://github.com/JCTec/iCalendarKit.git", .revision("58c1cbfad574b1b423ac4dedcb010f2c89e89b0b"))
+        .package(url: "https://github.com/nmdias/FeedKit.git", from: "9.1.2")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
@@ -107,7 +107,7 @@ let package = Package(
                 .product(name: "PerfectSysInfo", package: "PerfectSysInfo"),
                 .product(name: "SwiftPlot", package: "SwiftPlot"),
                 .product(name: "AGGRenderer", package: "SwiftPlot"),
-                .product(name: "iCalendarKit", package: "iCalendarKit"),
+                .product(name: "ICalendar", package: "swift-icalendar"),
                 .target(name: "D2MessageIO"),
                 .target(name: "D2Permissions"),
                 .target(name: "D2Script"),
